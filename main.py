124da2a9830d81f26e1bb999ca62addbc8e08f15
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-10-28 17:08:39 -0300

    refactor: :recycle: change values ​to constants

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ from message.main import message_world_boss_alert
 import discord
 
 
+ALERT_N_MINUTES_BEFORE_WORLD_BOSS_SPAWN = 10
+CHECK_BOSS_WORLD_EVERY_N_MINUTES = 1
+EMPTY_LIST = 0
+
+
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.users_notification = list()
-        # self.activate_alert.start()
 
     async def on_ready(self):
         print(f'[on] GW2 Bot')        
         self.activate_alert.start()
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
+    async def on_message(self, message):        
         if message.author.id == self.user.id:
             return
 
@@ -34,26 +37,21 @@ class MyClient(discord.Client):
                 self.users_notification.remove(user)                
                 await user.send('[off] World Boss notification!', mention_author=True)
 
-    @tasks.loop(minutes=1)  
+    @tasks.loop(minutes=CHECK_BOSS_WORLD_EVERY_N_MINUTES)  
     async def activate_alert(self):        
-        world_boss_time = addition_n_minutes(get_time(), 10)
+        world_boss_time = addition_n_minutes(get_time(), ALERT_N_MINUTES_BEFORE_WORLD_BOSS_SPAWN)
         world_boss_data = load_world_boss_data()
         
         try:
             avaliable_world_boss = world_boss_data[world_boss_time]
             message_alert = message_world_boss_alert(avaliable_world_boss)
 
-            if len(self.users_notification) != 0:
+            if len(self.users_notification) != EMPTY_LIST:
                 for member in self.users_notification:                                
                     await member.send(message_alert)  
         except Exception:
             pass
-        
-        # if len(self.users_notification) != 0:
-        #     for member in self.users_notification:            
-                
-        #         await member.send('happy codding')    
-
+ 
 
 intents = discord.Intents.default()
 intents.members = True
