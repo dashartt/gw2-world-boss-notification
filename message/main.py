7cd542e99469683b64750dd4df086c96c37adf0b
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-10-28 16:39:59 -0300

    feat: :sparkles: message world boss notification
    message template to alert user

---
+++ message/main.py
@@ -0,0 +1,22 @@
+def message_world_boss_alert(avaliable_world_boss):
+    informations = list()
+
+    informations.append("""
+# World Boss Notification
+
+## Avaliables:
+___________________________
+    """)
+
+    for world_boss in avaliable_world_boss:
+        informations.append(
+            f"""
+name: {world_boss['name']}
+starts at: {world_boss['start_time']}
+zone: {world_boss['zone']}
+area: {world_boss['area']}
+___________________________
+            """
+        )
+    
+    return "".join(informations)
