9066eefa41d3428e2bd8aab745289fbdd26eb70e
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-10-30 22:11:00 -0300

    feat: :sparkles: embed-shaped alert message
    instead a simple text

--- main.py
+++ main.py
@@ -29,30 +29,42 @@ class MyClient(discord.Client):
 
             if not user in self.users_notification:
                 self.users_notification.append(user)
-                await user.send('[on] World Boss notification!', mention_author=True)
+                embed_card=discord.Embed(title = '[on] World Boss notification!', color=0x29bd00)                
+                embed_card.set_footer(text="You will get the alerts")
+                await user.send(embed= embed_card)                
         
         if message.content.startswith('$stop'):
             user = message.author
 
             if user in self.users_notification:
                 self.users_notification.remove(user)                
-                await user.send('[off] World Boss notification!', mention_author=True)
+                embed_card=discord.Embed(title = '[off] World Boss notification!',color=0xc00c0c)                
+                embed_card.set_footer(text="You will not receive the alerts")
+                await user.send(embed=embed_card)
 
-    @tasks.loop(minutes=CHECK_BOSS_WORLD_EVERY_N_MINUTES)  
+    @tasks.loop(minutes=CHECK_BOSS_WORLD_EVERY_N_MINUTES)      
     async def activate_alert(self):        
         world_boss_time = addition_n_minutes(get_time(), ALERT_N_MINUTES_BEFORE_WORLD_BOSS_SPAWN)
         world_boss_data = load_world_boss_data()
         
         try:
             avaliable_world_boss = world_boss_data[world_boss_time]
-            message_alert = message_world_boss_alert(avaliable_world_boss)
-
+                    
             if len(self.users_notification) != EMPTY_LIST:
-                for member in self.users_notification:                                
-                    await member.send(message_alert)  
+                for member in self.users_notification:                              
+                    for world_boss in avaliable_world_boss:
+                        embed_card = discord.Embed(title=world_boss['name'])                                                 
+                        embed_card.set_thumbnail(url='https://media.discordapp.net/attachments/933408739480961086/1035643643840712704/NO8hnUi.jpg?width=960&height=540')
+                        embed_card.add_field(name='Starts at', value=world_boss['start_time'], inline=False)
+                        embed_card.add_field(name='Waypoint link', value=world_boss['waypoint_link'], inline=False)
+                        embed_card.add_field(name='Zone', value=world_boss['zone'], inline=False)
+                        embed_card.add_field(name='Area', value=world_boss['area'], inline=False)        
+                        # embed_card.set_image(url="https://media.discordapp.net/attachments/933408739480961086/1035643643840712704/NO8hnUi.jpg?width=960&height=540")                        
+
+                        await member.send(embed=embed_card)                        
         except Exception:
             pass
- 
+                
 
 load_dotenv()
 
